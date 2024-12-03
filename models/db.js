5c53be121fc01d81869c39260ff40ce1bb2e66cc
theusinshow <matheusmendes077@gmail.com>
2024-12-03 12:03:41 -0300

    atualização referente ao sonar cloud

--- models/db.js
+++ models/db.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql2');
 
-// Configuração da conexão com o MySQL
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -8,16 +7,14 @@ const db = mysql.createConnection({
   database: 'pc_builder',
 });
 
-// Conectar ao banco de dados
 (async () => {
   try {
     await db.connect();
     console.log('Conexão com o banco de dados estabelecida!');
   } catch (err) {
     console.error('Erro ao conectar ao banco de dados:', err.message);
-    // Use apenas console.error durante os testes em vez de process.exit(1)
     if (process.env.NODE_ENV !== 'test') {
-      process.exit(1);
+      process.exit(1); // Só encerra se não estiver no ambiente de teste
     }
   }
 })();
